bb3cc41147bed8500f8c3108099557497e49340b
peetc1 <cpeet@insight.rr.com>
2015-06-22 22:44:04 -0400

    adding tests for various hand types

--- PokerHand.Tests/DeckTest.cs
+++ PokerHand.Tests/DeckTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Collections.Specialized;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PokerHand.Business.Objects;
 
 namespace PokerHand.Tests
@@ -28,17 +30,59 @@ namespace PokerHand.Tests
             var deck = new DeckService();
 
             // execute
-            var left = deck.GetCardsLeft();
-
             for (var i = 0; i < 45; i++)
             {
                 deck.GetNextCard();
             }
 
-            var remaining = deck.GetCardsLeft();
+            var left = deck.GetCardsLeft();
 
             // validation
-            Assert.AreEqual(_fullDeck - 45, remaining);
+            Assert.AreEqual(_fullDeck - 45, left);
+        }
+
+        [TestMethod]
+        public void FullDeckAfterPullingAndShuffling()
+        {
+            // setup
+            var deck = new DeckService();
+
+            // execute
+            for (var i = 0; i < 45; i++)
+            {
+                deck.GetNextCard();
+            }
+
+            deck.Shuffle();
+            var left = deck.GetCardsLeft();
+
+            // validation
+            Assert.AreEqual(_fullDeck, left);
+        }
+
+        [TestMethod]
+        public void ErrorPullingToManyCards()
+        {
+            // setup
+            var deck = new DeckService();
+
+            var error = "";
+
+            // execute
+            try
+            {
+                for (var i = 0; i < 53; i++)
+                {
+                    deck.GetNextCard();
+                }
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+
+            // validation
+            Assert.AreEqual("No cards left in the deck", error);
         }
 
     }
---
+++ PokerHand.Tests/HandComparerServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokerHand.Business.Objects;
+
+namespace PokerHand.Tests
+{
+    [TestClass]
+    public class HandComparerServiceTest
+    {
+        // these pieces are always needed
+        private Player _p1 = new Player("p1");
+        private Player _p2 = new Player("p2");
+        
+
+
+        [TestMethod]
+        public void StraightFlushBeatsOtherHandTypes()
+        {
+            
+
+
+        }
+    }
+}
